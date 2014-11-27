df4bafb5da15043cbf91b593646eac09adf587cf
The Kix <kixi73@gmail.com>
2014-11-27 20:09:45 +0100

    dojo: add a list of todos and just display unformatted

--- src/cljs/clojure_dojo/core.cljs
+++ src/cljs/clojure_dojo/core.cljs
@@ -5,17 +5,28 @@
             [cljs.core.async :refer [put! chan <! timeout]]
             [clojure.string :as string]))
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(defonce app-state (atom {:text "Hello Chestnut!"
+                          :todos ["Milch kaufen" "Auto waschen"]}))
 
 
 (defn main []
   (om/root
     (fn [app owner]
       (reify
-
-        om/IRenderState
+       om/IRenderState
         (render-state [_ state]
           (dom/div nil
-                   (dom/h1 nil (:text app))))))
+                   (dom/h1 nil (:text app))
+                   (dom/ul nil
+                           (str
+                            (map
+                              (fn [todo]
+                               todo 
+                              )
+                              (:todos app))))))
+      )
+    )
     app-state
-    {:target (. js/document (getElementById "app"))}))
+    {:target (. js/document (getElementById "app"))}
+  )
+)
